57c1fdf36f67327f91b5f3959df4bc57a60111f4
Sean Price <sean@seanprice.io>
2022-02-19 22:02:53 -0500

    Add autosend prop to EmailVerificationForm

--- src/components/forms/email-verification/EmailVerificationForm.tsx
+++ src/components/forms/email-verification/EmailVerificationForm.tsx
@@ -1,4 +1,4 @@
-import { memo, VFC } from 'react';
+import { memo, useEffect, useState, VFC } from 'react';
 import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
@@ -8,34 +8,51 @@ import { useEmailVerificationForm, useSignOut } from '@sprice237/react-auth';
 import { ButtonContainer } from '$/components/ui/ButtonContainer';
 import { LoadingOverlay } from '$/components/ui/LoadingSpinner';
 
-export type EmailVerificationFormProps = Record<string, never>;
+export type EmailVerificationFormProps = {
+  autosend?: boolean;
+};
 
-export const EmailVerificationForm: VFC<EmailVerificationFormProps> = memo(() => {
-  const signOut = useSignOut();
-  const { error, inProgress, isComplete, submit } = useEmailVerificationForm();
+export const EmailVerificationForm: VFC<EmailVerificationFormProps> = memo(
+  ({ autosend = false }) => {
+    const signOut = useSignOut();
+    const [hasBeenSent, setHasBeenSent] = useState(false);
+    const { error, inProgress, isComplete, submit } = useEmailVerificationForm();
 
-  return (
-    <LoadingOverlay isLoading={inProgress}>
-      <Box mx={10} my={5}>
-        {error && <Alert severity="error">{error}</Alert>}
-        {isComplete && (
-          <Alert severity="success">Please check your email for a verification message.</Alert>
-        )}
-        <Box my={3}>
-          <Typography>
-            Your email address must be verified. Please click the button below to send a
-            verification email to your email address.
-          </Typography>
+    useEffect(() => {
+      if (isComplete) {
+        setHasBeenSent(true);
+      }
+    }, [isComplete]);
+
+    useEffect(() => {
+      if (autosend) {
+        submit();
+      }
+    }, [autosend]);
+
+    return (
+      <LoadingOverlay isLoading={inProgress}>
+        <Box mx={10} my={5}>
+          {error && <Alert severity="error">{error}</Alert>}
+          {isComplete && (
+            <Alert severity="success">Please check your email for a verification message.</Alert>
+          )}
+          <Box my={3}>
+            <Typography>
+              Your email address must be verified. Please click the button below to{' '}
+              {hasBeenSent ? 'resend' : 'send'} the verification email to your email address.
+            </Typography>
+          </Box>
+          <ButtonContainer>
+            <Button onClick={submit} variant="contained" color="primary">
+              {hasBeenSent ? 'Resend verification email' : 'Send verification email'}
+            </Button>
+            <Button onClick={signOut} component={Link} color="primary">
+              Back to login
+            </Button>
+          </ButtonContainer>
         </Box>
-        <ButtonContainer>
-          <Button onClick={submit} variant="contained" color="primary">
-            Send verification email
-          </Button>
-          <Button onClick={signOut} component={Link} color="primary">
-            Back to login
-          </Button>
-        </ButtonContainer>
-      </Box>
-    </LoadingOverlay>
-  );
-});
+      </LoadingOverlay>
+    );
+  }
+);
